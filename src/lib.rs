b491366d074eceb611d337760456e61276ce7fc4
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2016-05-14 21:10:01 +0200

    Fixed example

--- src/lib.rs
+++ src/lib.rs
@@ -51,8 +51,8 @@ impl Nmea {
     /// ```
     /// use nmea::Nmea;
     ///
-    /// let nmea= Nmea::new();
-    /// let gga = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76",
+    /// let mut nmea= Nmea::new();
+    /// let gga = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76";
     /// nmea.parse(gga).ok();
     /// println!("{}", nmea);
     /// ```
