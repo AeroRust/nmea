70ab4ec7d2f24aaf9c25e6c9996ab49af8f79f70
Jihyun Yu <yjh0502@gmail.com>
2020-06-13 18:09:14 +0900

    remove unwrap, add testcase

--- src/sentences/txt.rs
+++ src/sentences/txt.rs
@@ -1,9 +1,11 @@
+use arrayvec::ArrayString;
 use nom::character::complete::char;
-
 use nom::{bytes::complete::take_while, IResult};
 
 use super::utils::number;
 use crate::parse::NmeaSentence;
+
+const MAX_LEN: usize = 64;
 
 /// Parse TXT message from u-blox device
 ///
@@ -26,14 +28,32 @@ pub fn parse_txt(s: &NmeaSentence) -> Result<TxtData, String> {
                 kind.description().to_string()
             }
         })?;
-    Ok(ret)
+
+    let text_str = match std::str::from_utf8(ret.text) {
+        Ok(s) => s,
+        Err(_e) => {
+            return Err("txt: non-utf8 data".to_string());
+        }
+    };
+
+    let text = match ArrayString::from(text_str) {
+        Ok(s) => s,
+        Err(_e) => return Err("txt too long".to_string()),
+    };
+
+    Ok(TxtData {
+        count: ret.count,
+        seq: ret.seq,
+        text_ident: ret.text_ident,
+        text,
+    })
 }
 
 fn txt_str(s: &[u8]) -> IResult<&[u8], &[u8]> {
     take_while(|c| c != b',' && c != b'*')(s)
 }
 
-fn do_parse_txt(i: &[u8]) -> IResult<&[u8], TxtData> {
+fn do_parse_txt<'a>(i: &'a [u8]) -> IResult<&'a [u8], TxtData0<'a>> {
     let (i, count) = number::<u8>(i)?;
     let (i, _) = char(',')(i)?;
     let (i, seq) = number::<u8>(i)?;
@@ -44,11 +64,11 @@ fn do_parse_txt(i: &[u8]) -> IResult<&[u8], TxtData> {
 
     Ok((
         i,
-        TxtData {
+        TxtData0 {
             count,
             seq,
             text_ident,
-            text: std::str::from_utf8(text).unwrap().to_owned(),
+            text,
         },
     ))
 }
@@ -58,5 +78,49 @@ pub struct TxtData {
     pub count: u8,
     pub seq: u8,
     pub text_ident: u8,
-    pub text: String,
+    pub text: ArrayString<[u8; MAX_LEN]>,
+}
+
+struct TxtData0<'a> {
+    pub count: u8,
+    pub seq: u8,
+    pub text_ident: u8,
+    pub text: &'a [u8],
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::parse::parse_nmea_sentence;
+
+    #[test]
+    fn smoke_test_parse_txt() {
+        let s = parse_nmea_sentence(b"$GNTXT,01,01,02,u-blox AG - www.u-blox.com*4E").unwrap();
+        let txt = parse_txt(&s).unwrap();
+        assert_eq!(
+            TxtData {
+                count: 1,
+                seq: 1,
+                text_ident: 2,
+                text: ArrayString::from("u-blox AG - www.u-blox.com").unwrap(),
+            },
+            txt
+        );
+
+        let gsa_examples = [
+            "$GPTXT,01,01,02,u-blox ag - www.u-blox.com*50",
+            "$GPTXT,01,01,02,HW  UBX-G70xx   00070000 FF7FFFFFo*69",
+            "$GPTXT,01,01,02,ROM CORE 1.00 (59842) Jun 27 2012 17:43:52*59",
+            "$GPTXT,01,01,02,PROTVER 14.00*1E",
+            "$GPTXT,01,01,02,ANTSUPERV=AC SD PDoS SR*20",
+            "$GPTXT,01,01,02,ANTSTATUS=OK*3B",
+            "$GPTXT,01,01,02,LLC FFFFFFFF-FFFFFFFF-FFFFFFFF-FFFFFFFF-FFFFFFFD*2C",
+        ];
+        for line in &gsa_examples {
+            println!("we parse line '{}'", line);
+            let s = parse_nmea_sentence(line.as_bytes()).unwrap();
+            parse_txt(&s).unwrap();
+        }
+    }
 }
