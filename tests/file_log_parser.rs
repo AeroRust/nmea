c6b9c620109f781ac6d1d0b1383120183e012d95
Evgeniy A. Dushistov <dushistov@mail.ru>
2022-04-17 22:20:46 +0300

    move data for tests to separate directory
    this simplify cross-platform testing,
    when "cargo test" run tests on separate machine via "runner" config option

--- tests/file_log_parser.rs
+++ tests/file_log_parser.rs
@@ -5,6 +5,40 @@ use std::{
     path::Path,
 };
 
+#[test]
+fn test_parse_file_log() {
+    let res = process_file(&Path::new("tests").join("data").join("nmea1.log"))
+        .unwrap_or_else(|err| panic!("process file failed with error '{}'", err));
+
+    let expected: Vec<_> = BufReader::new(
+        File::open(&Path::new("tests").join("data").join("nmea1.log.expected")).unwrap(),
+    )
+    .lines()
+    .map(|v| v.unwrap())
+    .collect();
+    assert_eq!(expected, res);
+}
+
+#[test]
+fn test_parse_issue_2() {
+    let mut input =
+        BufReader::new(File::open(&Path::new("tests").join("data").join("nmea2.log")).unwrap());
+    let mut nmea = nmea::Nmea::new();
+    for _ in 0..100 {
+        let mut buffer = String::new();
+        let size = input.read_line(&mut buffer).unwrap();
+        eprintln!("buffer = {:?}", buffer);
+        if size > 0 {
+            if buffer.as_bytes()[0] == b'$' {
+                let _ = nmea.parse(&buffer);
+                println!("{:?}", nmea);
+            }
+        } else {
+            break;
+        }
+    }
+}
+
 fn err_to_string<E: Error>(e: E) -> String {
     e.to_string()
 }
@@ -23,36 +57,4 @@ fn process_file(n: &Path) -> Result<Vec<String>, String> {
         ret.push(format!("{:?}", parse_res));
     }
     Ok(ret)
-}
-
-#[test]
-fn test_parse_file_log() {
-    let res = process_file(&Path::new("tests").join("nmea1.log"))
-        .unwrap_or_else(|err| panic!("process file failed with error '{}'", err));
-
-    let expected: Vec<_> =
-        BufReader::new(File::open(&Path::new("tests").join("nmea1.log.expected")).unwrap())
-            .lines()
-            .map(|v| v.unwrap())
-            .collect();
-    assert_eq!(expected, res);
-}
-
-#[test]
-fn test_parse_issue_2() {
-    let mut input = BufReader::new(File::open(&Path::new("tests").join("nmea2.log")).unwrap());
-    let mut nmea = nmea::Nmea::new();
-    for _ in 0..100 {
-        let mut buffer = String::new();
-        let size = input.read_line(&mut buffer).unwrap();
-        eprintln!("buffer = {:?}", buffer);
-        if size > 0 {
-            if buffer.as_bytes()[0] == b'$' {
-                let _ = nmea.parse(&buffer);
-                println!("{:?}", nmea);
-            }
-        } else {
-            break;
-        }
-    }
 }
