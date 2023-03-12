34942d8a3e41ef723994b8e5f16f874c9d48ee80
ekuinox <depkey@me.com>
2023-03-12 18:04:02 +0900

    Add functions and comments

--- src/sentences/zda.rs
+++ src/sentences/zda.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveTime;
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime};
 use nom::{
     bytes::complete::take,
     character::complete::char,
@@ -10,14 +10,103 @@ use crate::{parse::NmeaSentence, sentences::utils::parse_hms, Error, SentenceTyp
 
 use super::utils::{parse_num, parse_number_in_range};
 
+/// ZDA - Time & Date - UTC, day, month, year and local time zone
+///
+/// <https://gpsd.gitlab.io/gpsd/NMEA.html#_zda_time_date_utc_day_month_year_and_local_time_zone>
+///
+/// ```text
+///        1         2  3  4    5  6  7
+///        |         |  |  |    |  |  |
+/// $--ZDA,hhmmss.ss,xx,xx,xxxx,xx,xx*hh<CR><LF>
+/// ```
+///
+/// 1. UTC time (hours, minutes, seconds, may have fractional subseconds)
+/// 2. Day, 01 to 31
+/// 3. Month, 01 to 12
+/// 4. Year (4 digits)
+/// 5. Local zone description, 00 to +- 13 hours
+/// 6. Local zone minutes description, 00 to 59, apply same sign as local hours
+/// 7. Checksum
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ZdaData {
     pub utc_time: Option<NaiveTime>,
     pub day: Option<u8>,
     pub month: Option<u8>,
     pub year: Option<u16>,
-    pub local_zone: Option<i8>,
-    pub local_zone_minutes_description: Option<i8>,
+    pub local_zone_hours: Option<i8>,
+    pub local_zone_minutes: Option<i8>,
+}
+
+impl ZdaData {
+    /// Get UTC date by `day`, `month` and `year` fields.
+    /// Returns `None` if any field is `None.
+    pub fn utc_date(&self) -> Option<NaiveDate> {
+        if let Some(((day, month), year)) = self.day.zip(self.month).zip(self.year) {
+            NaiveDate::from_ymd_opt(year.into(), month.into(), day.into())
+        } else {
+            None
+        }
+    }
+
+    /// Get UTC date time by `utc_time`, `day`, `month`, and `year` fields.
+    /// Returns `None` if any field is `None.
+    pub fn utc_date_time(&self) -> Option<NaiveDateTime> {
+        self.utc_time.and_then(|utc_time| {
+            self.utc_date()
+                .map(|utc_date| NaiveDateTime::new(utc_date, utc_time))
+        })
+    }
+
+    /// Get `chrono::FixedOffset` by `local_zone_hours` and `local_zone_minutes` fields.
+    /// Return `Some` if either `local_zone_hours` or `local_zone_minutes` is `Some`.
+    pub fn offset(&self) -> Option<FixedOffset> {
+        let hours = self.local_zone_hours.map(i32::from);
+        let minutes = self.local_zone_minutes.map(i32::from);
+        match (hours, minutes) {
+            (Some(h), Some(m)) => FixedOffset::east_opt(((h * 60) + m) * 60),
+            (Some(h), None) => FixedOffset::east_opt(h * 60 * 60),
+            (None, Some(m)) => FixedOffset::east_opt(m * 60),
+            (None, None) => None,
+        }
+    }
+
+    /// Caluculate local datetime
+    /// Returns `None` if any field is `None.
+    pub fn local_date_time(&self) -> Option<DateTime<FixedOffset>> {
+        self.utc_date_time()
+            .zip(self.offset())
+            .and_then(|(date_time, offset)| date_time.and_local_timezone(offset).single())
+    }
+}
+
+/// # Parse ZDA message
+///
+/// From gpsd/driver_nmea0183.c
+///
+/// ```text
+/// $GPZDA,160012.71,11,03,2004,-1,00*7D
+/// ```
+///
+/// 1) UTC time (hours, minutes, seconds, may have fractional subsecond)
+/// 2) Day, 01 to 31
+/// 3) Month, 01 to 12
+/// 4) Year (4 digits)
+/// 5) Local zone description, 00 to +- 13 hours
+/// 6) Local zone minutes description, apply same sign as local hours
+/// 7) Checksum
+///
+/// Note: some devices, like the u-blox ANTARIS 4h, are known to ship ZDAs
+/// with some fields blank under poorly-understood circumstances (probably
+/// when they don't have satellite lock yet).
+pub fn parse_zda(sentence: NmeaSentence) -> Result<ZdaData, Error> {
+    if sentence.message_id != SentenceType::ZDA {
+        Err(Error::WrongSentenceHeader {
+            expected: SentenceType::ZDA,
+            found: sentence.message_id,
+        })
+    } else {
+        Ok(do_parse_zda(sentence.data)?.1)
+    }
 }
 
 fn do_parse_zda(i: &str) -> IResult<&str, ZdaData> {
@@ -32,11 +121,11 @@ fn do_parse_zda(i: &str) -> IResult<&str, ZdaData> {
     let (i, _) = comma(i)?;
     let (i, minus) = opt(char('-'))(i)?;
     let signum = minus.map(|_| -1).unwrap_or(1);
-    let (i, local_zone) = opt(|i| parse_number_in_range::<i8>(i, 0, 13))(i)?;
-    let local_zone = local_zone.map(|z| z * signum);
+    let (i, local_zone_hours) = opt(|i| parse_number_in_range::<i8>(i, 0, 13))(i)?;
+    let local_zone_hours = local_zone_hours.map(|z| z * signum);
     let (i, _) = comma(i)?;
-    let (i, local_zone_minutes_description) = opt(|i| parse_number_in_range::<i8>(i, -59, 59))(i)?;
-    let local_zone_minutes_description = local_zone_minutes_description.map(|m| m * signum);
+    let (i, local_zone_minutes) = opt(|i| parse_number_in_range::<i8>(i, -59, 59))(i)?;
+    let local_zone_minutes = local_zone_minutes.map(|m| m * signum);
 
     Ok((
         i,
@@ -45,21 +134,10 @@ fn do_parse_zda(i: &str) -> IResult<&str, ZdaData> {
             day,
             month,
             year,
-            local_zone,
-            local_zone_minutes_description,
+            local_zone_hours,
+            local_zone_minutes,
         },
     ))
-}
-
-pub fn parse_zda(sentence: NmeaSentence) -> Result<ZdaData, Error> {
-    if sentence.message_id != SentenceType::ZDA {
-        Err(Error::WrongSentenceHeader {
-            expected: SentenceType::ZDA,
-            found: sentence.message_id,
-        })
-    } else {
-        Ok(do_parse_zda(sentence.data)?.1)
-    }
 }
 
 #[cfg(test)]
@@ -85,8 +163,8 @@ mod tests {
                 day: Some(11),
                 month: Some(3),
                 year: Some(2004),
-                local_zone: Some(-1),
-                local_zone_minutes_description: Some(0),
+                local_zone_hours: Some(-1),
+                local_zone_minutes: Some(0),
             },
         );
 
@@ -98,8 +176,8 @@ mod tests {
                 day: None,
                 month: None,
                 year: None,
-                local_zone: None,
-                local_zone_minutes_description: None,
+                local_zone_hours: None,
+                local_zone_minutes: None,
             },
         );
 
@@ -111,8 +189,8 @@ mod tests {
                 day: None,
                 month: None,
                 year: None,
-                local_zone: Some(-1),
-                local_zone_minutes_description: Some(-5),
+                local_zone_hours: Some(-1),
+                local_zone_minutes: Some(-5),
             },
         );
 
@@ -124,9 +202,42 @@ mod tests {
                 day: None,
                 month: None,
                 year: None,
-                local_zone: None,
-                local_zone_minutes_description: Some(21),
+                local_zone_hours: None,
+                local_zone_minutes: Some(21),
             },
+        );
+    }
+
+    #[test]
+    fn test_parse_zda_datetime() {
+        let s = parse_nmea_sentence("$GPZDA,160012.71,11,03,2004,-1,00*7D").unwrap();
+        assert_eq!(s.checksum, s.calc_checksum());
+        assert_eq!(s.checksum, 0x7d);
+        let zda_data = parse_zda(s).unwrap();
+        assert_eq!(
+            zda_data.utc_date(),
+            Some(NaiveDate::from_ymd_opt(2004, 3, 11).unwrap())
+        );
+        assert_eq!(
+            zda_data.utc_date_time(),
+            Some(NaiveDateTime::new(
+                NaiveDate::from_ymd_opt(2004, 3, 11).unwrap(),
+                NaiveTime::from_hms_milli_opt(16, 00, 12, 710).unwrap()
+            ))
+        );
+        assert_eq!(
+            zda_data.offset(),
+            Some(FixedOffset::east_opt(-1 * 60 * 60).unwrap())
+        );
+        assert_eq!(
+            zda_data.local_date_time(),
+            Some(DateTime::from_local(
+                NaiveDateTime::new(
+                    NaiveDate::from_ymd_opt(2004, 3, 11).unwrap(),
+                    NaiveTime::from_hms_milli_opt(16, 00, 12, 710).unwrap()
+                ),
+                FixedOffset::east_opt(-1 * 60 * 60).unwrap()
+            ))
         );
     }
 }
